eccb8900107ab1e30366e5a83a840566a1ef2868
zhangwei95 <574150903@qq.com>
2020-05-06 15:18:43 +0800

    print ip port

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,18 +7,43 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 @SpringBootApplication
 public class TestApplication {
     protected static final Logger logger = LoggerFactory.getLogger(TestApplication.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext run = SpringApplication.run(TestApplication.class, args);
-        System.out.println(DateUtils.timestamp2LocalDateTime(Long.valueOf("1567242193000")));
+
+
+        Environment env = run.getEnvironment();
+        logger.info(
+                "\n----------------------------------------------------------\n\t"
+                        + "Application '{}' is running! Access URLs:\n\t"
+                        + "Local: \t\thttp://127.0.0.1:{}\n\t"
+                        + "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+
+        String configServerStatus = env.getProperty("configserver.status");
+        logger.info(
+                "\n----------------------------------------------------------\n\t"
+                        + "Config Server: \t{}\n----------------------------------------------------------",
+                configServerStatus == null
+                        ? "Not found or not setup for this application"
+                        : configServerStatus);
+
         HelloBean helloBean =(HelloBean)run.getBean("helloBean");
+
         helloBean.sayHello();
+
         logger.trace("I am trace log.");
         logger.debug("I am debug log.");
         logger.info("I am info log.");
@@ -27,5 +52,7 @@ public class TestApplication {
 
 
     }
+
+
 
 }
